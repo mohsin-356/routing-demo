d1c245b69fd14c360afb39cd68194858c5a156c6
mohsin-356 <mohsinsaeed356@gmail.com>
2025-10-01 18:57:11 +0500

    Refactor Home and Products pages to enhance navigation with Link components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 export const metadata = {
   title: "Saeed's App"
 };
 export default function Home() {
     return (
         <main>
-            <h1>Home page</h1>
+            <h1>Welcome to Mohsin&apos;s App Home</h1>
+            <Link href="/">Home</Link>
+            <Link href="/products">Products</Link>
+            <Link href="/blog">Blog</Link>
         </main>
     );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,20 @@
+import Link from "next/link";
+
 export default function Products() {
     return (
         <main>
             <h1>Products page</h1>
             here will be a list of products
             <ul>
-                <li>Product 1</li>
-                <li>Product 2</li>
-                <li>Product 3</li>
+                <Link href="/products/1">
+                    <li>Product 1</li>
+                </Link>
+                <Link href="/products/2">
+                    <li>Product 2</li>
+                </Link>
+                <Link href="/products/3" replace>
+                    <li>Product 3</li>
+                </Link>
             </ul>
         </main>
     );
