3de7b535755ae7ea72bbda86d2ddb5cae965ae6f
mohsin-356 <mohsinsaeed356@gmail.com>
2025-10-03 19:12:09 +0500

    Refactor Home component to wrap navigation links in list items for improved structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,22 @@ export default function Home() {
     return (
         <main>
             <h1>Welcome to Mohsin&apos;s App Home</h1>
-            <Link href="/">Home</Link>
-            <Link href="/products">Products</Link>
-            <Link href="/blog">Blog</Link>
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+            <li>
+                <Link href="/products">Products</Link>
+            </li>
+            <li>
+                <Link href="/blog">Blog</Link>
+            </li>
+            <li>
+                <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
+            </li>
+            <li>
+                <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
+            </li>
+            
         </main>
     );
 }
