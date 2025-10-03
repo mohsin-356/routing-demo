0fba72a3497491351797aa4da491ae6a4b56b2c7
mohsin-356 <mohsinsaeed356@gmail.com>
2025-10-03 19:07:15 +0500

    Update package dependencies and restructure PostCSS configuration; enhance authentication layout with navigation links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 export const metadata = {
   title: "Mohsin's App",
   description: "This is my app description",
@@ -6,7 +7,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
