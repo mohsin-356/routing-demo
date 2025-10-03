1a03fd1dc506100c434d68e65ea6b6e94e503ffb
mohsin-356 <mohsinsaeed356@gmail.com>
2025-10-03 19:47:44 +0500

    Add NewsArticle component to display article details with language options

--- src/app/articles/[articleId]/page.tsx
+++ src/app/articles/[articleId]/page.tsx
@@ -1,18 +1,20 @@
 import Link from "next/link";
 
-export default function NewsArticle(
+export default async function NewsArticle(
     { params, searchParams }:
         { params: Promise<{ articleID: string }>, searchParams: { lang?: "en" | "fr" | "es" } }
 ) {
     console.log(params);
     console.log(searchParams);
+    const { articleID } = await params;
+    const { lang } = await searchParams;
     return <div>
-        <h1>News Article ID</h1>
-        <p>Reading in Language</p>
+        <h1>News Article {articleID}</h1>
+        <p>Reading in Language: {lang}</p>
         <div>
-            <Link href="/articles/breaking-news-123?lang=en">English</Link>
-            <Link href="/articles/breaking-news-123?lang=es">Spanish</Link>
-            <Link href="/articles/breaking-news-123?lang=fr">French</Link>
+            <Link href={`/articles/${articleID}?${lang}`}>English</Link>
+            <Link href={`/articles/${articleID}?${lang}`}>Spanish</Link>
+            <Link href={`/articles/${articleID}?${lang}`}>French</Link>
         </div>
     </div>
 }
